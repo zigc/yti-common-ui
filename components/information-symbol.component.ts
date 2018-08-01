0010d2d7c08175605e4578ca070bb0abae9520c3
JereVeijalainen <jereveijalainen82@gmail.com>
2018-08-01 14:31:37 +0300

    Add style for information symbol component

--- components/information-symbol.component.ts
+++ components/information-symbol.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-information-symbol',
+  styleUrls: ['./information-symbol.component.scss'],
   template: `<i *ngIf="infoText" class="fa fa-info-circle" [ngbTooltip]="infoText | translate"></i>`
 })
 export class InformationSymbolComponent {
