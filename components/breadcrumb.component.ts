23a14f91222ed3d6bbe0c4739a9ace7eee1e3732
Tuomo Soinio <tuomo.soinio@gofore.com>
2018-08-21 16:33:35 +0300

    YTI-994: Remove demo stuff.

--- components/breadcrumb.component.ts
+++ components/breadcrumb.component.ts
@@ -9,47 +9,52 @@ import {Router} from "@angular/router";
   template: `
     <ol class="breadcrumb">
       <li class="breadcrumb-item" [class.active]="active" *ngFor="let breadcrumb of (location | async); let active = last; let i = index">
-        <a [id]=" i + '_breadcrumb_link'" *ngIf="(!active || linkActive) && breadcrumb.route" [routerLink]="breadcrumb.route" [skipLocationChange]="active">
+        <!-- Normal ancestor link -->
+        <a [id]=" i + '_breadcrumb_link'" *ngIf="!active && breadcrumb.route" [routerLink]="breadcrumb.route">
           <span *ngIf="breadcrumb.localizationKey">{{breadcrumb.localizationKey | translate}}</span>
           <span *ngIf="breadcrumb.localizationKey && breadcrumb.label">:</span>
           <span *ngIf="breadcrumb.label">{{breadcrumb.label | translateValue:true}}</span>
           <span *ngIf="breadcrumb.value">{{breadcrumb.value}}</span>
         </a>
-        <a [id]=" i + '_breadcrumb_link2'" *ngIf="active && linkActive && breadcrumb.route" [href]="breadcrumb.route!.join('/')">
-          -
-          <span *ngIf="breadcrumb.localizationKey">{{breadcrumb.localizationKey | translate}}</span>
-          <span *ngIf="breadcrumb.localizationKey && breadcrumb.label">:</span>
-          <span *ngIf="breadcrumb.label">{{breadcrumb.label | translateValue:true}}</span>
-          <span *ngIf="breadcrumb.value">{{breadcrumb.value}}</span>
-        </a>
-        <a [id]=" i + '_breadcrumb_link3'" *ngIf="active && linkActive && refreshPath && breadcrumb.route" [routerLink]="refreshPath!.concat(breadcrumb.route)" [skipLocationChange]="true">
-          -
-          <span *ngIf="breadcrumb.localizationKey">{{breadcrumb.localizationKey | translate}}</span>
-          <span *ngIf="breadcrumb.localizationKey && breadcrumb.label">:</span>
-          <span *ngIf="breadcrumb.label">{{breadcrumb.label | translateValue:true}}</span>
-          <span *ngIf="breadcrumb.value">{{breadcrumb.value}}</span>
-        </a>
-        <a [id]=" i + '_breadcrumb_link4'" *ngIf="active && linkActive && refreshPath && breadcrumb.route" [href]="breadcrumb.route!.join('/')" (click)="refresh($event, breadcrumb.route)">
-          -
-          <span *ngIf="breadcrumb.localizationKey">{{breadcrumb.localizationKey | translate}}</span>
-          <span *ngIf="breadcrumb.localizationKey && breadcrumb.label">:</span>
-          <span *ngIf="breadcrumb.label">{{breadcrumb.label | translateValue:true}}</span>
-          <span *ngIf="breadcrumb.value">{{breadcrumb.value}}</span>
-        </a>
+        <!-- Non-linked active view indicator (or anything without route) -->
         <span *ngIf="(active && !linkActive) || !breadcrumb.route">
           <span *ngIf="breadcrumb.localizationKey">{{breadcrumb.localizationKey | translate}}</span>
           <span *ngIf="breadcrumb.localizationKey && breadcrumb.label">:</span>
           <span *ngIf="breadcrumb.label">{{breadcrumb.label | translateValue:true}}</span>
           <span *ngIf="breadcrumb.value">{{breadcrumb.value}}</span>
         </span>
+        <!-- Active view refresh link using full page load (i.e., standard html link, skip routing) -->
+        <a [id]=" i + '_breadcrumb_link'" *ngIf="active && linkActive && !refreshPath && breadcrumb.route" [href]="breadcrumb.route!.join('/')">
+          <span *ngIf="breadcrumb.localizationKey">{{breadcrumb.localizationKey | translate}}</span>
+          <span *ngIf="breadcrumb.localizationKey && breadcrumb.label">:</span>
+          <span *ngIf="breadcrumb.label">{{breadcrumb.label | translateValue:true}}</span>
+          <span *ngIf="breadcrumb.value">{{breadcrumb.value}}</span>
+        </a>
+        <!-- Active view refresh link using transitional route target -->
+        <a [id]=" i + '_breadcrumb_link'" *ngIf="active && linkActive && refreshPath && breadcrumb.route" [href]="breadcrumb.route!.join('/')" (click)="refresh($event, breadcrumb.route)">
+          <span *ngIf="breadcrumb.localizationKey">{{breadcrumb.localizationKey | translate}}</span>
+          <span *ngIf="breadcrumb.localizationKey && breadcrumb.label">:</span>
+          <span *ngIf="breadcrumb.label">{{breadcrumb.label | translateValue:true}}</span>
+          <span *ngIf="breadcrumb.value">{{breadcrumb.value}}</span>
+        </a>
       </li>
     </ol>
   `
 })
 export class BreadcrumbComponent {
-
+  /**
+   * Location (navigation path) observable to show on breadcrumb.
+   */
   @Input() location: Observable<Location[]>;
+  /**
+   * Set to 'true' to make active view indicator a clickable link, with the component re-initialization intent. It is recommended to also give working refreshPath,
+   * as full page reload is used otherwise.
+   */
   @Input() linkActive?: boolean;
+  /**
+   * Route path that closes the components that need re-initialization on active view link activation. Also known as "the dummy component route". If this is given
+   * together with linkActive, then clicking on the active view indicator navigates to this path, and immediately back to the original route.
+   */
   @Input() refreshPath?: string[];
 
   constructor(private router : Router) {
