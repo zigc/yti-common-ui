c52840ff2762da73f5ceebc514fcbc0acafd2578
Tuukka Laitinen <work.ictlaitinen@gmail.com>
2018-09-13 08:53:40 +0300

    YTI-1074 - Fixed the info-ball component so that there is now again a space between the symbol and the header that it pertains to.

--- components/information-symbol.component.ts
+++ components/information-symbol.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-information-symbol',
   styleUrls: ['./information-symbol.component.scss'],
-  template: `<i *ngIf="infoText" class="fa fa-info-circle" [ngbTooltip]="infoText | translate"></i>`
+  template: `&nbsp;<i *ngIf="infoText" class="fa fa-info-circle" [ngbTooltip]="infoText | translate"></i>`
 })
 export class InformationSymbolComponent {
   @Input() infoText: string;
