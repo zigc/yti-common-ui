d64dbc87c8d86045d40a0064720b58477ed46267
Tuomo Soinio <tuomo.soinio@gofore.com>
2018-10-02 15:49:10 +0300

    Fix IconComponent.

--- components/icon.component.ts
+++ components/icon.component.ts
@@ -1,5 +1,11 @@
-import {Component, Input} from '@angular/core';
-import {getVocabularyTypeMaterialIcon, getDataModelingMaterialIcon, getInformationDomainSvgIcon, getUiMaterialIcon} from '../utils/icons';
+import { Component, Input } from '@angular/core';
+import {
+  getDataModelingMaterialIcon,
+  getInformationDomainSvgIcon,
+  getUiMaterialIcon,
+  getVocabularyTypeMaterialIcon,
+  MaterialIconDef
+} from '../utils/icons';
 
 export enum IconCategory {
   VocabularyType = "VOCABULARY_TYPE",
@@ -16,11 +22,13 @@ export enum IconCategory {
   styleUrls: ['./icon.component.scss'],
   template: `
     <ng-container [ngSwitch]="category">
-      <i *ngSwitchCase="CategoryType.VocabularyType" class="yti-icon material-icons">{{vocabularyTypeIconName(id)}}</i>
-      <i *ngSwitchCase="CategoryType.DataModeling" class="yti-icon">{{dataModelingIconName(id)}}</i>
+      <i *ngSwitchCase="CategoryType.VocabularyType"
+         class="yti-icon material-icons {{vocabularyTypeIconDef(id).colorClass}}">{{vocabularyTypeIconDef(id).name}}</i>
+      <i *ngSwitchCase="CategoryType.DataModeling"
+         class="yti-icon material-icons {{dataModelingIconDef(id).colorClass}}">{{dataModelingIconDef(id).name}}</i>
       <img *ngSwitchCase="CategoryType.InformationDomain" class="yti-icon" [src]="informationDomainIconSrc(id)">
-      <i *ngSwitchCase="CategoryType.UI" class="yti-icon">{{uiIconName(id)}}</i>
-      <i *ngSwitchDefault class="material-icons">warning</i>
+      <i *ngSwitchCase="CategoryType.UI" class="yti-icon {{uiIconDef(id).colorClass}}">{{uiIconDef(id).name}}</i>
+      <i *ngSwitchDefault class="yti-icon material-icons icon-danger">warning</i>
     </ng-container>
   `
 })
@@ -28,16 +36,20 @@ export class IconComponent {
   @Input() category: IconCategory;
   @Input() id: string;
   CategoryType = IconCategory;
-  vocabularyTypeIconName(id: string): string {
+
+  vocabularyTypeIconDef(id: string): MaterialIconDef {
     return getVocabularyTypeMaterialIcon(id);
   }
-  dataModelingIconName(id: string): string {
+
+  dataModelingIconDef(id: string): MaterialIconDef {
     return getDataModelingMaterialIcon(id);
   }
+
   informationDomainIconSrc(id: string): any {
     return getInformationDomainSvgIcon(id);
   }
-  uiIconName(id: string): string {
+
+  uiIconDef(id: string): MaterialIconDef {
     return getUiMaterialIcon(id);
   }
 }
