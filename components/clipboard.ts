b7eecd4fb9be82f9adbb868b19c7c60d2583dc36
Tuomo Soinio <tuomo.soinio@gofore.com>
2018-11-24 17:01:01 +0200

    YTI-56: An attempt to generalize codelist-ui's clipboard component.

---
+++ components/clipboard.ts
@@ -0,0 +1,70 @@
+import { Component, Input, ViewChild } from '@angular/core';
+import { NgbPopover, NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
+import { ClipboardService } from 'ngx-clipboard';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-clipboard',
+  styleUrls: ['./clipboard.scss'],
+  template: `
+      <a *ngIf="displayValue && showAsLink" target="_blank" href="{{displayValue}}">{{displayValue}}</a>
+      <span *ngIf="displayValue && !showAsLink">{{displayValue}}</span>
+      <i class="far fa-copy"
+         #t="ngbTooltip"
+         ngbTooltip="{{'Copy value to clipboard' | translate:translateParams}}"
+         #p="ngbPopover"
+         ngbPopover="{{'Copied to clipboard' | translate}}"
+         ngxClipboard
+         (click)="clickToolTip()"></i>
+  `
+})
+export class ClipboardComponent {
+
+  @ViewChild('t') public tooltip: NgbTooltip;
+  @ViewChild('p') public popover: NgbPopover;
+
+  /**
+   * If displayValue is given, then it is shown before icon as either a link or text. If only displayValue is given then
+   * it is used as the value too. Either of the displayValue or value must be given.
+   */
+  @Input() displayValue?: string;
+  /**
+   * Whether to assume the displayValue to be a link and render it as such.
+   */
+  @Input() showAsLink = false;
+  /**
+   * Value to copy to clipboard, or then a provider used to construct the value. If not given then displayValue is used as the value,
+   * and thus must be given.
+   */
+  @Input() value?: string | (() => string);
+  /**
+   * Description of copied content used in a tooltip. If not given then displayValue is tried, and finally a generic "value" is used.
+   */
+  @Input() description?: string;
+  
+  constructor(private clipboardService: ClipboardService,
+              private translateService: TranslateService) {
+  }
+
+  get translateParams() {
+    return {
+      value: this.description ? this.description : this.displayValue ? this.displayValue : this.translateService.instant('value')
+    };
+  }
+
+  clickToolTip() {
+    const content = typeof this.value === 'string' ? this.value : this.value ? this.value() : this.displayValue;
+    if (content) {
+      this.clipboardService.copyFromContent(content);
+      this.tooltip.close();
+      setTimeout(() => {
+        this.popover.close();
+      }, 1500);
+    } else {
+      this.tooltip.close();
+      setTimeout(() => {
+        this.popover.close();
+      }, 0);
+    }
+  }
+}
--- index.ts
+++ index.ts
@@ -1,32 +1,34 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {TranslateModule} from '@ngx-translate/core';
-import {BreadcrumbComponent} from './components/breadcrumb.component';
-import {FooterComponent} from './components/footer.component';
-import {BackButtonComponent} from './components/back-button.component';
-import {AjaxLoadingIndicatorComponent} from './components/ajax-loading-indicator.component';
-import {AjaxLoadingIndicatorSmallComponent} from './components/ajax-loading-indicator-small.component';
-import {ErrorModalComponent, ErrorModalService} from './components/error-modal.component';
-import {ConfirmationModalComponent, ConfirmationModalService} from './components/confirmation-modal.component';
-import {LoginModalComponent, LoginModalService} from './components/login-modal.component';
-import {MenuComponent} from './components/menu.component';
-import {DropdownComponent} from './components/dropdown.component';
-import {FilterDropdownComponent} from './components/filter-dropdown.component';
-import {StatusDropdownComponent} from './components/status-dropdown.component';
-import {UserService, AUTHENTICATED_USER_ENDPOINT} from './services/user.service';
-import {TranslateValuePipe, LOCALIZER} from './pipes/translate-value.pipe';
-import {PopoverCloseComponent} from './components/popover-close.component';
-import {HighlightPipe} from './pipes/highlight.pipe';
-import {AccordionChevronComponent} from './components/accordion-chevron.component';
-import {KeysPipe} from './pipes/keys.pipe';
-import {NotificationDirective, NotificationWindowComponent} from './components/notification.component';
-import {RequiredSymbolComponent} from './components/required-symbol.component';
-import {InformationSymbolComponent} from './components/information-symbol.component';
-import {StatusComponent} from "./components/status.component";
-import {IconComponent} from "./components/icon.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { BreadcrumbComponent } from './components/breadcrumb.component';
+import { FooterComponent } from './components/footer.component';
+import { BackButtonComponent } from './components/back-button.component';
+import { AjaxLoadingIndicatorComponent } from './components/ajax-loading-indicator.component';
+import { AjaxLoadingIndicatorSmallComponent } from './components/ajax-loading-indicator-small.component';
+import { ErrorModalComponent, ErrorModalService } from './components/error-modal.component';
+import { ConfirmationModalComponent, ConfirmationModalService } from './components/confirmation-modal.component';
+import { LoginModalComponent, LoginModalService } from './components/login-modal.component';
+import { MenuComponent } from './components/menu.component';
+import { DropdownComponent } from './components/dropdown.component';
+import { FilterDropdownComponent } from './components/filter-dropdown.component';
+import { StatusDropdownComponent } from './components/status-dropdown.component';
+import { UserService, AUTHENTICATED_USER_ENDPOINT } from './services/user.service';
+import { TranslateValuePipe, LOCALIZER } from './pipes/translate-value.pipe';
+import { PopoverCloseComponent } from './components/popover-close.component';
+import { HighlightPipe } from './pipes/highlight.pipe';
+import { AccordionChevronComponent } from './components/accordion-chevron.component';
+import { KeysPipe } from './pipes/keys.pipe';
+import { NotificationDirective, NotificationWindowComponent } from './components/notification.component';
+import { RequiredSymbolComponent } from './components/required-symbol.component';
+import { InformationSymbolComponent } from './components/information-symbol.component';
+import { StatusComponent } from "./components/status.component";
+import { IconComponent } from "./components/icon.component";
+import { ClipboardComponent } from './components/clipboard';
+import { ClipboardModule } from 'ngx-clipboard';
 
 declare var require: any;
 
@@ -57,7 +59,8 @@ const components = [
   RequiredSymbolComponent,
   InformationSymbolComponent,
   StatusComponent,
-  IconComponent
+  IconComponent,
+  ClipboardComponent
 ];
 
 @NgModule({
@@ -75,7 +78,8 @@ const components = [
     ReactiveFormsModule,
     NgbModule,
     TranslateModule,
-    RouterModule
+    RouterModule,
+    ClipboardModule
   ],
   providers: [
     ErrorModalService,
