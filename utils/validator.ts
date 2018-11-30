ac46689472d6f0c2cd4611c7b1eecaf89cb81150
Tuomo Soinio <tuomo.soinio@gofore.com>
2018-11-30 16:10:38 +0200

    Added some structural requirements to URL validation regexp.

--- utils/validator.ts
+++ utils/validator.ts
@@ -18,6 +18,7 @@ export function validateLanguage(control: AbstractControl): ValidationErrors | n
   };
 }
 
-// TODO: This is just a starting point dummy pattern which should be replaced with something real, while continuing to accept
-//  internationalized URLs, parameters, ports, etc.
-export const httpOrHttpsUrlRegex = /^https?:\/\/\S+$/;
+// TODO: The following is just an early attempt to force some structural correctness. Internationalized URLs and JavaScript's inability
+//  to support unicode character classes cause so much trouble that the task is un-pleasing at best. Also, it might make sense to consider
+//  disallowing userinfo, localhost and IP addresses.
+export const httpOrHttpsUrlRegex = /^https?:\/\/(?:[^\s\/@]+@)?(:?localhost|\[[a-fA-F0-9:.]+\]|[^\s\/@:.?#\[\]]+(?:\.[^\s\/@:.?#\[\]]+)+)(?::\d+)?(?:\/\S*)?$/;
