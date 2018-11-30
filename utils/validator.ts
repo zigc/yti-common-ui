13dd607830dc06607cc56fd7e390abebcec2a8d5
Tuomo Soinio <tuomo.soinio@gofore.com>
2018-11-30 13:04:24 +0200

    Added preliminary (dummy) regexp for HTTP(S) URL validation.

--- utils/validator.ts
+++ utils/validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 import { contains } from './array';
 import { ietfLanguageTags } from '..';
 
-export function requiredList(control: AbstractControl): ValidationErrors|null {
+export function requiredList(control: AbstractControl): ValidationErrors | null {
   return Object.values(control.value).length > 0 ? null : {
     required: {
       valid: false
@@ -10,10 +10,14 @@ export function requiredList(control: AbstractControl): ValidationErrors|null {
   };
 }
 
-export function validateLanguage(control: AbstractControl): ValidationErrors|null {
-  return contains(ietfLanguageTags, control.value)  ? null : {
+export function validateLanguage(control: AbstractControl): ValidationErrors | null {
+  return contains(ietfLanguageTags, control.value) ? null : {
     validateLanguage: {
       valid: false
     }
   };
 }
+
+// TODO: This is just a starting point dummy pattern which should be replaced with something real, while continuing to accept
+//  internationalized URLs, parameters, ports, etc.
+export const httpOrHttpsUrlRegex = /^https?:\/\/\S+$/;
